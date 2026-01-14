2210e2053fdd93b7bc00c335de06a02eb3acbc97
ZayuRa9 <ngsoanng@gmail.com>
2026-01-14 23:36:05 +0700

    Update function startRoll(){.js

--- function startRoll(){.js
+++ function startRoll(){.js
@@ -1,14 +1,1 @@
-function startRoll(){
-    if(rolling || locked) return;
-    rolling = true;
 
-    tick.currentTime = 0;
-    tick.play();
-    numberEl.classList.add("shake");
-
-    timer = setInterval(() => {
-        // random CỰC NHANH – tạo hiệu ứng loạn
-        const fake = Math.floor(Math.random() * 900) + 1;
-        numberEl.innerText = String(fake).padStart(3,"0");
-    }, 30); // 🔥 30ms = rất nhanh
-}
